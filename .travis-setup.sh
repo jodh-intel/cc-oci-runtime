82f71265e0a9db8e7ccf46325f2389f9a43da1da
James O. D. Hunt <james.o.hunt@intel.com>
2016-09-13 17:43:19 +0100

    CI: Only build dependencies not already available.
    The build dependencies take a long time to build so only build them if
    they don't already exist.
    
    The approach taken is simple - compare the repository version of
    "versions.txt" with a version installed at the time the cached versions
    of the build dependencies were installed. If the versions differ at all
    (only a simple string comparison is performed), rebuild that build
    dependency.
    
    This is a precursor to adding cache support to the Travis build.
    
    Signed-off-by: James O. D. Hunt <james.o.hunt@intel.com>

--- .travis-setup.sh
+++ .travis-setup.sh
@@ -20,7 +20,13 @@
 set -e -x
 
 cwd=$(cd `dirname "$0"`; pwd -P)
+
+local_versions="$cwd/versions.txt"
+cached_versions="/usr/local/lib/cached_versions.txt"
+
 source $cwd/versions.txt
+
+[ -e "${cached_versions}" ] && source "$cached_versions"
 
 # Ensure "make install" as root can find clang
 #
@@ -40,153 +46,184 @@ mkdir cor-dependencies
 pushd cor-dependencies
 
 # Build glib
-glib_major=`echo $glib_version | cut -d. -f1`
-glib_minor=`echo $glib_version | cut -d. -f2`
-curl -L -O "$gnome_dl/glib/${glib_major}.${glib_minor}/glib-${glib_version}.tar.xz"
-tar -xf "glib-${glib_version}.tar.xz"
-pushd "glib-${glib_version}"
-./configure --disable-silent-rules
-make -j5
-sudo make install
-popd
+if [ "${glib_version}" != "${cached_glib_version}" ]
+then
+    glib_major=`echo $glib_version | cut -d. -f1`
+    glib_minor=`echo $glib_version | cut -d. -f2`
+    curl -L -O "$gnome_dl/glib/${glib_major}.${glib_minor}/glib-${glib_version}.tar.xz"
+    tar -xf "glib-${glib_version}.tar.xz"
+    pushd "glib-${glib_version}"
+    ./configure --disable-silent-rules
+    make -j5
+    sudo make install
+    popd
+fi
 
 # Build json-glib
-json_major=`echo $json_glib_version | cut -d. -f1`
-json_minor=`echo $json_glib_version | cut -d. -f2`
-curl -L -O "$gnome_dl/json-glib/${json_major}.${json_minor}/json-glib-${json_glib_version}.tar.xz"
-tar -xf "json-glib-${json_glib_version}.tar.xz"
-pushd "json-glib-${json_glib_version}"
-./configure --disable-silent-rules
-make -j5
-sudo make install
+if [ "${json_glib_version}" != "${cached_json_glib_version}" ]
+then
+    json_major=`echo $json_glib_version | cut -d. -f1`
+    json_minor=`echo $json_glib_version | cut -d. -f2`
+    curl -L -O "$gnome_dl/json-glib/${json_major}.${json_minor}/json-glib-${json_glib_version}.tar.xz"
+    tar -xf "json-glib-${json_glib_version}.tar.xz"
+    pushd "json-glib-${json_glib_version}"
+    ./configure --disable-silent-rules
+    make -j5
+    sudo make install
 popd
+fi
 
 # Build check
 # We need to build check as the check version in the OS used by travis isn't
 # -pedantic safe.
-curl -L -O "https://github.com/libcheck/check/releases/download/${check_version}/check-${check_version}.tar.gz"
-tar -xf "check-${check_version}.tar.gz"
-pushd "check-${check_version}"
-./configure
-make -j5
-sudo make install
-popd
-
+if [ "${check_version}" != "${cached_check_version}" ]
+then
+    curl -L -O "https://github.com/libcheck/check/releases/download/${check_version}/check-${check_version}.tar.gz"
+    tar -xf "check-${check_version}.tar.gz"
+    pushd "check-${check_version}"
+    ./configure
+    make -j5
+    sudo make install
+    popd
+fi
 
 # Install bats
-git clone https://github.com/sstephenson/bats.git
-pushd bats
-sudo ./install.sh /usr/local
-popd
+# (Note: no version, just check if the binary is there)
+if [ ! -x /usr/local/bin/bats ]
+then
+    git clone https://github.com/sstephenson/bats.git
+    pushd bats
+    sudo ./install.sh /usr/local
+    popd
+fi
 
 # build gcc (required for qemu-lite)
-curl -L -O "http://mirrors.kernel.org/gnu/gcc/gcc-${gcc_version}/gcc-${gcc_version}.tar.bz2"
-tar xf "gcc-${gcc_version}.tar.bz2"
-pushd "gcc-${gcc_version}"
-./configure --enable-languages=c --disable-multilib --prefix="/usr/local/gcc-${gcc_version}"
-make -j5
-sudo make install
 export PATH="/usr/local/gcc-${gcc_version}:$PATH"
-popd
+if [ "${gcc_version}" != "${cached_gcc_version}" ]
+then
+    curl -L -O "http://mirrors.kernel.org/gnu/gcc/gcc-${gcc_version}/gcc-${gcc_version}.tar.bz2"
+    tar xf "gcc-${gcc_version}.tar.bz2"
+    pushd "gcc-${gcc_version}"
+    ./configure --enable-languages=c --disable-multilib --prefix="/usr/local/gcc-${gcc_version}"
+    make -j5
+    sudo make install
+    popd
+fi
 
 # build qemu-lite
-curl -L -O "https://github.com/01org/qemu-lite/archive/${qemu_lite_version}.tar.gz"
-tar xf "${qemu_lite_version}.tar.gz"
-pushd "qemu-lite-${qemu_lite_version}"
-CC="gcc" ./configure \
-    --disable-bluez \
-    --disable-brlapi \
-    --disable-bzip2 \
-    --disable-curl \
-    --disable-curses \
-    --disable-debug-tcg \
-    --disable-fdt \
-    --disable-glusterfs \
-    --disable-gtk \
-    --disable-libiscsi \
-    --disable-libnfs \
-    --disable-libssh2 \
-    --disable-libusb \
-    --disable-linux-aio \
-    --disable-lzo \
-    --disable-opengl \
-    --disable-qom-cast-debug \
-    --disable-rbd \
-    --disable-rdma \
-    --disable-sdl \
-    --disable-seccomp \
-    --disable-slirp \
-    --disable-snappy \
-    --disable-spice \
-    --disable-strip \
-    --disable-tcg-interpreter \
-    --disable-tcmalloc \
-    --disable-tools \
-    --disable-tpm \
-    --disable-usb-redir \
-    --disable-uuid \
-    --disable-vnc \
-    --disable-vnc-{jpeg,png,sasl} \
-    --disable-vte \
-    --disable-xen \
-    --enable-attr \
-    --enable-cap-ng \
-    --enable-kvm \
-    --enable-virtfs \
-    --target-list=x86_64-softmmu \
-    --extra-cflags="-fno-semantic-interposition -O3 -falign-functions=32" \
-    --prefix=/usr/local \
-    --datadir=/usr/local/share/qemu-lite \
-    --libdir=/usr/local/lib64/qemu-lite \
-    --libexecdir=/usr/local/libexec/qemu-lite
-make -j5
-sudo make install
-popd
+if [ "${qemu_lite_version}" != "${cached_qemu_lite_version}" ]
+then
+    curl -L -O "https://github.com/01org/qemu-lite/archive/${qemu_lite_version}.tar.gz"
+    tar xf "${qemu_lite_version}.tar.gz"
+    pushd "qemu-lite-${qemu_lite_version}"
+    CC="gcc" ./configure \
+        --disable-bluez \
+        --disable-brlapi \
+        --disable-bzip2 \
+        --disable-curl \
+        --disable-curses \
+        --disable-debug-tcg \
+        --disable-fdt \
+        --disable-glusterfs \
+        --disable-gtk \
+        --disable-libiscsi \
+        --disable-libnfs \
+        --disable-libssh2 \
+        --disable-libusb \
+        --disable-linux-aio \
+        --disable-lzo \
+        --disable-opengl \
+        --disable-qom-cast-debug \
+        --disable-rbd \
+        --disable-rdma \
+        --disable-sdl \
+        --disable-seccomp \
+        --disable-slirp \
+        --disable-snappy \
+        --disable-spice \
+        --disable-strip \
+        --disable-tcg-interpreter \
+        --disable-tcmalloc \
+        --disable-tools \
+        --disable-tpm \
+        --disable-usb-redir \
+        --disable-uuid \
+        --disable-vnc \
+        --disable-vnc-{jpeg,png,sasl} \
+        --disable-vte \
+        --disable-xen \
+        --enable-attr \
+        --enable-cap-ng \
+        --enable-kvm \
+        --enable-virtfs \
+        --target-list=x86_64-softmmu \
+        --extra-cflags="-fno-semantic-interposition -O3 -falign-functions=32" \
+        --prefix=/usr/local \
+        --datadir=/usr/local/share/qemu-lite \
+        --libdir=/usr/local/lib64/qemu-lite \
+        --libexecdir=/usr/local/libexec/qemu-lite
+    make -j5
+    sudo make install
+    popd
+fi
 
 # install kernel + Clear Containers image
 mkdir artifacts
 pushd artifacts
 clr_release=$(curl -L https://download.clearlinux.org/latest)
 clr_kernel_base_url="https://download.clearlinux.org/releases/${clr_release}/clear/x86_64/os/Packages"
-sudo mkdir -p /usr/share/clear-containers/
+
+clr_assets_dir=/usr/share/clear-containers/
+sudo mkdir -p "$clr_assets_dir"
 
 # find newest containers kernel
-kernel=$(curl -l -s -L "${clr_kernel_base_url}" |\
+clr_kernel=$(curl -l -s -L "${clr_kernel_base_url}" |\
     grep -o "linux-container-[0-9][0-9.-]*\.x86_64.rpm" |\
     sort -u)
 
 # download kernel
-curl -L -O "${clr_kernel_base_url}/${kernel}"
+if [ ! -e "${clr_assets_dir}/${clr_kernel}" ]
+then
+    curl -L -O "${clr_kernel_base_url}/${clr_kernel}"
 
-# install kernel
-# (note: cpio on trusty does not support "-D")
-rpm2cpio "$kernel"| (cd / && sudo cpio -idv)
+    # install kernel
+    # (note: cpio on trusty does not support "-D")
+    rpm2cpio "${clr_kernel}"| (cd / && sudo cpio -idv)
+fi
 
 # download image
-clr_image_url="https://download.clearlinux.org/current/clear-${clr_release}-containers.img.xz"
-clr_image=$(basename "$clr_image_url")
-for file in "${clr_image_url}-SHA512SUMS" "${clr_image_url}"
-do
-    curl -L -O "$file"
-done
-
-# verify image
-sha512sum -c "${clr_image}-SHA512SUMS"
-
-# unpack image
-unxz "${clr_image}"
-
-# remove suffix
-clr_image=${clr_image/.xz/}
-
-# install image
-sudo install "${clr_image}" /usr/share/clear-containers/
+if [ ! -e "${clr_assets_dir}/${clr_image}" ]
+then
+    clr_image_url="https://download.clearlinux.org/current/clear-${clr_release}-containers.img.xz"
+    clr_image_compressed=$(basename "$clr_image_url")
+    
+    # uncompressed image name
+    clr_image=${clr_image_compressed/.xz/}
+    
+    for file in "${clr_image_url}-SHA512SUMS" "${clr_image_url}"
+    do
+        curl -L -O "$file"
+    done
+    
+    # verify image
+    sha512sum -c "${clr_image_compressed}-SHA512SUMS"
+    
+    # unpack image
+    unxz "${clr_image_compressed}"
+    
+    # install image
+    sudo install "${clr_image}" "${clr_assets_dir}"
+fi
 
 # change kernel+image ownership
-sudo chown -R $USER /usr/share/clear-containers/
+sudo chown -R $USER "${clr_assets_dir}"
 
 # create image symlink (kernel will already have one)
-(cd /usr/share/clear-containers && sudo ln -s "${clr_image}" clear-containers.img)
+clr_image_link=clear-containers.img
+if [ ! -e "${clr_assets_dir}/${clr_image_link}" ]
+then
+    (cd "${clr_assets_dir}" && sudo ln -s "${clr_image}" "${clr_image_link}")
+fi
 
 mkdir docker
 pushd docker
@@ -213,3 +250,14 @@ sudo tar -C /tmp/bundle/rootfs -xvf "$fs"
 popd
 
 popd
+
+# copy the "versions database" to the cache directory if it's not
+# already there. Since /usr/local is our Travis cache directory, we can
+# identify on the next build if these build dependencies need to
+# rebuilt. Most of time they won't (but will if a version is bumped in
+# the versions database).
+sudo mkdir -p $(dirname "${cached_versions}")
+egrep -v "^(\#|$)" "${local_versions}" |\
+    sed 's/^/cached_/g' |\
+    sudo tee "${cached_versions}"
+sudo chmod 444 "${cached_versions}"
