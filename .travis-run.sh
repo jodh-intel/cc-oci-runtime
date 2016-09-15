c57c1e574ffa377f252335fb9b75ca49d63f8919
James O. D. Hunt <james.o.hunt@intel.com>
2016-09-15 17:06:04 +0100

    CI: Build and run tests inside docker.
    A docker image provides a consistent environment to run the build and
    tests inside.
    
    Travis only provides Ubuntu Trusty meaning to support running in that
    environment, it is necessary to build glib, json-glib, qemu-lite *and*
    gcc6 (required by qemu-lite). This is possible, but the time taken to
    build gcc6 and the amount of output generated exceed Travis job limits.
    
    By using a docker image that already has the required library versions,
    qemu-lite and the Clear Containers assets, the Travis build becomes a
    lot simpler.
    
    Signed-off-by: James O. D. Hunt <james.o.hunt@intel.com>

--- .travis-run.sh
+++ .travis-run.sh
@@ -17,13 +17,67 @@
 #  along with this program; if not, write to the Free Software
 #  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
-set -e
+set -e -x
 
-./autogen.sh \
-    --enable-cppcheck \
-    --enable-valgrind \
-    --disable-valgrind-helgrind \
-    --disable-valgrind-drd \
-    --disable-silent-rules
+#./autogen.sh \
+#    --enable-cppcheck \
+#    --enable-valgrind \
+#    --disable-valgrind-helgrind \
+#    --disable-valgrind-drd \
+#    --disable-silent-rules
+#
+#make -j5 CFLAGS=-Werror check
 
-make -j5 CFLAGS=-Werror check
+# Name of docker image used to build in
+image=jodh/cor-build-fedora
+
+# additional build flags
+CFLAGS=-Werror
+
+# arguments to pass to the build
+args=""
+
+# Clear Containers assets
+args="$args --with-cc-image=/usr/share/clear-containers/clear-containers.img"
+args="$args --with-cc-kernel=/usr/share/clear-containers/vmlinux.container"
+args="$args --with-tests-bundle-path=/var/lib/oci/bundle"
+
+# valgrind (memcheck) is essential, but helgrind and drd fail due to
+# suspected glib thread issues.
+#
+# See: https://github.com/01org/cc-oci-runtime/issues/214
+args="$args --enable-valgrind"
+args="$args --disable-valgrind-helgrind"
+args="$args --disable-valgrind-drd"
+
+# code coverage
+args="$args --enable-code-coverage"
+args="$args --with-gcov=lcov"
+
+# static analysis
+args="$args --enable-cppcheck"
+
+# miscellaneous
+args="$args --disable-silent-rules"
+
+# unlikely in cloud scenarios, but if VT-x support is available, use it! :-)
+grep -q vmx /proc/cpuinfo || args="$args --without-kvm-support"
+
+# Share the source directory into the container, configure and run the
+# tests twice - once as a non-privileged user, then as root. This is
+# required since the behaviour is slightly different for each.
+#
+# Notes:
+#
+# - the security option which is required to make use of ptrace(2)
+#   under docker.
+# - the test runs are not run multi-job since with "-j" the output is
+#   intermingled and hence confusing.
+docker run -ti \
+    --security-opt=seccomp:unconfined \
+    -v $PWD:/home/cor \
+    "$image" \
+    /bin/bash -c "./autogen.sh $args && \
+        make -j5 CFLAGS=$CFLAGS && \
+        make check && \
+        sudo make check"
